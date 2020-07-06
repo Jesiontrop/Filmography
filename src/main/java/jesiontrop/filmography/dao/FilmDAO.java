dd1c58ad5cc689af1e9eff978795bd418ba8497f
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-07-06 14:13:22 +0300

    Initial commit

---
+++ src/main/java/jesiontrop/filmography/dao/FilmDAO.java
@@ -0,0 +1,14 @@
+package jesiontrop.filmography.dao;
+
+import jesiontrop.filmography.model.Film;
+
+import java.util.List;
+
+public interface FilmDAO {
+    List<Film> allFilms(int page);
+    int filmsCount();
+    void add(Film film);
+    void delete(Film film);
+    void edit(Film film);
+    Film getById(int id);
+}
---
+++ src/main/java/jesiontrop/filmography/service/FilmService.java
@@ -0,0 +1,15 @@
+package jesiontrop.filmography.service;
+
+import jesiontrop.filmography.model.Film;
+
+import java.util.List;
+
+public interface FilmService {
+    List<Film> allFilms(int page);
+    int filmsCount();
+    void add(Film film);
+    void delete(Film film);
+    void edit(Film film);
+    Film getById(int id);
+}
+
